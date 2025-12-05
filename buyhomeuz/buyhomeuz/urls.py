e4bdb72f8e4519172200c5b65898af0f11b77419
Shin Igor <wiut2024id21974@gmail.com>
2025-12-05 21:22:09 +0500

    modified:   buyhomeuz/buyhomeuz/urls.py

--- buyhomeuz/buyhomeuz/urls.py
+++ buyhomeuz/buyhomeuz/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
@@ -13,6 +15,8 @@ urlpatterns = [
     path('analytics/', include('analytics.urls')),
     path('api/', include('api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+# if settings.DEBUG:
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
