d19128f161f94e85c1d635bb9fb25dba578883bb
Shin Igor <wiut2024id21974@gmail.com>
2025-12-05 21:34:38 +0500

    modified:   buyhomeuz/buyhomeuz/settings.py

--- buyhomeuz/buyhomeuz/settings.py
+++ buyhomeuz/buyhomeuz/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c*lf)pq#)u9ovuio9$4pfkc5o7w%$kfhb=tl=-!xid!98i#@yd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
