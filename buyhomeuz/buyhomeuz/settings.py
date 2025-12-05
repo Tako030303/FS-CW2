7e542a16fa70bbbb8a53df0d40e64b16a79125c9
Shin Igor <wiut2024id21974@gmail.com>
2025-12-05 21:07:38 +0500

    modified:   buyhomeuz/buyhomeuz/settings.py

--- buyhomeuz/buyhomeuz/settings.py
+++ buyhomeuz/buyhomeuz/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c*lf)pq#)u9ovuio9$4pfkc5o7w%$kfhb=tl=-!xid!98i#@yd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
